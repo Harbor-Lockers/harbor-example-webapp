8cb09476f97f9480b55da0186a5f036e5323e31c
Dylan Bozarth <dylanabozarth@gmail.com>
2025-05-08 08:24:25 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,109 @@
+import express from "express";
+import fetch from "node-fetch";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+// This is for the access token request
+const Access_token_url =
+  "https://accounts.sandbox.harborlockers.com/realms/harbor/protocol/openid-connect/token";
+const DropoffApi =
+  "https://api.sandbox.harborlockers.com/api/v1/locker-open-requests/dropoff-locker-request";
+const pickupApi =
+  "https://api.sandbox.harborlockers.com/api/v1/locker-open-requests/pickup-locker-request";
+let token = "";
+
+const params = new URLSearchParams();
+params.append("grant_type", "client_credentials");
+params.append("scope", "service_provider");
+params.append("client_id", `${process.env.CLIENT_ID}`);
+params.append("client_secret", `${process.env.CLIENT_SECRET}`);
+
+// get an access token
+app.post("/access-token", async (req, res) => {
+  console.log("access-token route hit");
+
+  const bodyParams = new URLSearchParams({
+    grant_type: "client_credentials",
+    scope: "service_provider",
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+  });
+
+  try {
+    const response = await fetch(Access_token_url, {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: bodyParams.toString(),
+    });
+    const data = await response.json();
+    token = data.access_token;
+    console.log("token is", token);
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// call the locker open api
+app.post("/dropoff-request", async (req, res) => {
+  try {
+    const response = await fetch(DropoffApi, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        locationId: 27, // use your location id, this can be found on harbor admin
+        lockerTypeId: 1, // locker types, 1 for small, 2 for medium, 3 for large 
+        requireLowLocker: false,
+        returnUrl: "https://yourwebsite.com/successpage", // where to send the user after the dropoff 
+        clientInfo: "Customer name/order number", 
+        payload: {},
+      }),
+    });
+
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+//call the pickup api
+app.post("/pickup-request", async (req, res) => {
+  try {
+    const { lockerId } = req.body;
+
+    const response = await fetch(pickupApi, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        lockerId, 
+        returnUrl: "",
+        clientInfo: "",
+        payload: {},
+      }),
+    });
+
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Harbor Lockers example web app
+
+### Set up your .env file
+`rename the env.example file to .env and fill in the values with your client_id and client_secret. Remember not to commit this file!`
+
+### Install the dependencies 
+`npm install  or yarn install`
+
+### Get your location Id from harbor admin
+`Or use one of our apis. Here's a few examples: https://api.sandbox.harborlockers.com/docs#/locations/get_locations_page`
+
+
+### Start the server
+`node main.js`
+
+
+
+
+
+
